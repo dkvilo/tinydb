1e60db9a0b2a2dfd060e7f72aba8b904ebfe0993
David Kviloria <dkviloria@gmail.com>
2024-09-11 19:13:03 +0400

    fix: safer command buffer parsing

--- tinydb_tcp_client_handler.c
+++ tinydb_tcp_client_handler.c
@@ -1,17 +1,17 @@
+#include <arpa/inet.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 
-#include "tinydb_database.h"
-#include "tinydb_context.h"
-#include "tinydb_log.h"
-#include "tinydb_tcp_client_handler.h"
-#include "tinydb_query_parser.h"
-#include "tinydb_command_executor.h"
 #include "config.h"
+#include "tinydb_command_executor.h"
+#include "tinydb_context.h"
+#include "tinydb_database.h"
+#include "tinydb_log.h"
+#include "tinydb_query_parser.h"
+#include "tinydb_tcp_client_handler.h"
 
 extern Database* db;
 extern RuntimeContext* context;
@@ -19,39 +19,33 @@ extern RuntimeContext* context;
 void
 TCP_Client_Handler(void* socket_desc)
 {
-  int sock = *(int*)socket_desc;
+  int32_t sock = *(int32_t*)socket_desc;
   char buffer[COMMAND_BUFFER_SIZE];
-  int read_size;
-  
-#if defined(SEND_PROMPT)
-  const char* prompt = "> ";
-  if (write(sock, prompt, strlen(prompt)) == -1) {
-    DB_Log(DB_LOG_ERROR, "TCP_SERVER Failed to send prompt, closing connection.");
-    close(sock);
-    free(socket_desc);
-    return;
-  }
-#endif
+  ssize_t read_size;
 
-  while ((read_size = recv(sock, buffer, COMMAND_BUFFER_SIZE, 0)) > 0) {
-    buffer[strcspn(buffer, "\r\n")] = 0; // newline
+  while (1) {
+
+    memset(buffer, 0, COMMAND_BUFFER_SIZE);
+    read_size = recv(sock, buffer, COMMAND_BUFFER_SIZE - 1, 0);
+
+    if (read_size <= 0)
+      break;
+
+    buffer[read_size] = '\0';
+    buffer[strcspn(buffer, "\r\n")] = 0;
+
     ParsedCommand* cmd = Parse_Command(buffer);
-
     if (cmd != NULL) {
       Execute_Command(sock, cmd, db);
       Free_Parsed_Command(cmd);
     } else {
-      write(sock, "Invalid command\n", strlen("Invalid command\n"));
+      const char* error_msg = "Invalid command\n";
+      if (write(sock, error_msg, strlen(error_msg)) == -1) {
+        DB_Log(DB_LOG_ERROR,
+               "TCP_SERVER Failed to send error message, closing connection.");
+        break;
+      }
     }
-
-    memset(buffer, 0, COMMAND_BUFFER_SIZE);
-#if defined(SEND_PROMPT)
-    if (write(sock, prompt, strlen(prompt)) == -1) {
-      DB_Log(DB_LOG_ERROR, "TCP_SERVER Failed to send prompt, closing connection.");
-      break;
-    }
-#endif
-
   }
 
   if (read_size == 0) {
