c3fa91a6879f0e1e6e9a2128ef945d14a84fe87d
David Kviloria <dkviloria@gmail.com>
2024-09-12 13:47:43 +0400

    feat: simple json and indented serialization for testing

--- tinydb_utils.h
+++ tinydb_utils.h
@@ -15,4 +15,23 @@ Append_To_Buffer(char** buffer,
                  size_t* buffer_size,
                  size_t* buffer_len);
 
+void
+Escape_String(char* dest, const char* src);
+
+void
+Add_Indentation(char* dest, int32_t level);
+
+// following functions are temporary and they are used for testing
+char*
+Serialize_DB_Object_ToJSON(DB_Object* obj);
+
+
+void
+Serialize_DB_Object_ToListStyle(DB_Object* obj,
+                                char* buffer,
+                                int32_t indent_level);
+
+char*
+Serialize_DB_Object_ToListStyleWrapper(DB_Object* obj);
+
 #endif // __TINY_DB_UTILS
