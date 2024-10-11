b4051e94dba29cc1fb72c1c0d9b3fd24eb81e18f
dkvilo <dkviloria@gmail.com>
2024-10-12 02:18:13 +0400

    fix: use bitmask instead of values

--- tinydb_acl.h
+++ tinydb_acl.h
@@ -5,9 +5,9 @@
 
 typedef enum DB_ACCESS_LEVEL
 {
-  DB_READ = 1 << 1,
-  DB_WRITE = 2 << 1,
-  DB_DELETE = 3 << 1
+  DB_READ = 1 << 0,
+  DB_WRITE = 1 << 1,
+  DB_DELETE = 1 << 2
 } DB_ACCESS_LEVEL;
 
 typedef struct DB_Access
