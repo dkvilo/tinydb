d9a5719dab7df9c5e29a492ad9a78b332ec74828
dkvilo <dkviloria@gmail.com>
2024-10-12 02:01:15 +0400

    feat: integrate lexer in command parser

--- tinydb_query_parser.c
+++ tinydb_query_parser.c
@@ -5,108 +5,70 @@
 
 #include "tinydb_query_parser.h"
 
-static TOKEN
-detect_token_type(const char* token)
+TOKEN
+To_Command_Token(LEX_TOKEN ltok)
 {
-  int is_number = 1;
-  int decimal_point_found = 0;
-  size_t length = strlen(token);
-
-  if (token[0] == '"' && token[length - 1] == '"') {
-    return TOKEN_STRING;
-  }
-
-  for (size_t i = 0; i < length; ++i) {
-    if (token[i] == '.') {
-      if (decimal_point_found) {
-        is_number = 0;
-        break;
-      }
-      decimal_point_found = 1;
-    } else if (!isdigit(token[i])) {
-      is_number = 0;
+  switch (ltok) {
+    case LEX_TOKEN_STRING:
+    case LEX_TOKEN_IDENTIFIER:
+      return TOKEN_STRING;
       break;
-    }
+    case LEX_TOKEN_NUMBER:
+      return TOKEN_NUMBER;
+      break;
+    default:
+      return TOKEN_STRING;
+      break;
   }
-  return is_number ? TOKEN_NUMBER : TOKEN_STRING;
-}
-
-static char*
-trim_quotes(char* str)
-{
-  if (str == NULL)
-    return NULL;
-  size_t len = strlen(str);
-  if (len > 1 && str[0] == '"' && str[len - 1] == '"') {
-    str[len - 1] = '\0';
-    return str + 1;
-  }
-  return str;
 }
 
 ParsedCommand*
-Parse_Command(const char* input)
+Parse_Command(char* buffer, size_t buffer_size, size_t* total_read)
 {
   ParsedCommand* cmd = calloc(1, sizeof(ParsedCommand));
-  if (!cmd)
-    return NULL;
-
-  char* input_copy = strdup(input);
-  if (!input_copy) {
-    free(cmd);
+  if (!cmd) {
     return NULL;
   }
 
-  char* saveptr;
-  char* token = strtok_r(input_copy, " ", &saveptr);
-  if (token) {
-    cmd->command = strdup(token);
-    if (!cmd->command)
-      goto cleanup;
-  }
+  cmd->lex = (Lexer){ 0 };
+  Lexer_Reset(&cmd->lex);
+  Lexer_Lex(&cmd->lex, (const uint8_t*)buffer, strlen(buffer));
 
-  char* key = strtok_r(NULL, " ", &saveptr);
-  if (key) {
-    cmd->argv[cmd->argc] = strdup(key);
-    if (!cmd->argv[cmd->argc])
-      goto cleanup;
-    cmd->types[cmd->argc] = TOKEN_STRING;
-    cmd->argc++;
+  // reset the buffer
+  *(total_read) = 0;
+  memset(buffer, 0, buffer_size);
 
-    char* value = strtok_r(NULL, "", &saveptr);
-    if (value) {
-      while (*value == ' ')
-        value++;
-      if (value[0] == '"') {
-        char* end_quote = strrchr(value, '"');
-        if (end_quote) {
-          *end_quote = '\0';
-          cmd->argv[cmd->argc] = strdup(value + 1);
-        } else {
-          cmd->argv[cmd->argc] = strdup(value);
-        }
-      } else {
-        cmd->argv[cmd->argc] = strdup(value);
+  if (cmd->lex.token_count > 0) {
+    for (size_t i = 0; i < cmd->lex.token_count; ++i) {
+      Token t = cmd->lex.tokens[i];
+      if (t.type == LEX_TOKEN_EOF)
+        continue;
+
+      if (i == 0 && t.type == LEX_TOKEN_COMMAND) {
+        cmd->command = strdup(t.value);
+        continue;
       }
-      if (!cmd->argv[cmd->argc])
-        goto cleanup;
-      cmd->types[cmd->argc] = detect_token_type(cmd->argv[cmd->argc]);
+
+      if (!cmd->command)
+        break;
+      cmd->argv[cmd->argc] = strdup(t.value);
+      cmd->types[cmd->argc] = To_Command_Token(t.type);
       cmd->argc++;
     }
   }
 
-  free(input_copy);
   return cmd;
-
-cleanup:
-  Free_Parsed_Command(cmd);
-  free(input_copy);
-  return NULL;
 }
 
 void
 Free_Parsed_Command(ParsedCommand* cmd)
 {
+  // free the lexer stuff
+  for (size_t i = 0; i < cmd->lex.token_count; i++) {
+    Lexer_Free_Token_Value(&cmd->lex.tokens[i]);
+  }
+
+  // free the cmd stuff
   if (cmd) {
     free(cmd->command);
     for (int i = 0; i < cmd->argc; i++) {
