8301a214837a4795092248f6e0264c85e40d54b9
David Kviloria <dkviloria@gmail.com>
2025-03-13 13:22:02 +0400

    feat: bg snapshoting process

--- tinydb_snapshot.h
+++ tinydb_snapshot.h
@@ -1,7 +1,8 @@
-#ifndef __TINY_DB_SNAPSHOT
-#define __TINY_DB_SNAPSHOT
+#ifndef __TINY_DB_SNAPSHOT_H
+#define __TINY_DB_SNAPSHOT_H
 
 #include "tinydb_context.h"
+#include <stdint.h>
 
 #define TINYDB_SIGNATURE "TINYDB"
 #define TINYDB_VERSION "0.0.1"
@@ -15,4 +16,15 @@ Import_Snapshot(RuntimeContext* ctx, const char* filename);
 void
 Print_Runtime_Context(RuntimeContext* ctx);
 
-#endif // __TINY_DB_SNAPSHOT
+int32_t
+Start_Background_Snapshot(RuntimeContext* ctx,
+                          int interval_seconds,
+                          const char* filename);
+
+int32_t
+Stop_Background_Snapshot(RuntimeContext* ctx);
+
+int32_t
+Set_Snapshot_Interval(RuntimeContext* ctx, int interval_seconds);
+
+#endif // __TINY_DB_SNAPSHOT_H
